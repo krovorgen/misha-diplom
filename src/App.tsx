37e6394b139e2d556781d23b1d48245241bc8449
maksim <krovorgen@gmail.com>
2023-04-19 19:09:59 +0300

    initial

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,39 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { Button } from '@alfalab/core-components/button';
+import { Swiper, SwiperSlide } from 'swiper/react';
+import { Autoplay, EffectFade } from 'swiper';
 
-function App() {
+import car1 from './images/1.png';
+import car2 from './images/2.png';
+import car3 from './images/3.png';
+import car4 from './images/4.png';
+
+export const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <Swiper
+        effect={'fade'}
+        slidesPerView={1}
+        loop={true}
+        autoplay={{
+          delay: 500,
+          disableOnInteraction: false,
+        }}
+        modules={[EffectFade, Autoplay]}>
+        <SwiperSlide>
+          <img src={car1} alt="car" />
+        </SwiperSlide>
+        <SwiperSlide>
+          <img src={car2} alt="car" />
+        </SwiperSlide>
+        <SwiperSlide>
+          <img src={car3} alt="car" />
+        </SwiperSlide>
+        <SwiperSlide>
+          <img src={car4} alt="car" />
+        </SwiperSlide>
+      </Swiper>
+      <Button view="primary">ad</Button>
     </div>
   );
-}
-
-export default App;
+};

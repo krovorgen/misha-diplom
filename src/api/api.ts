1ccc55eb32ecfdf2f6a08ed727dd36bc25f1b25e
maksim <krovorgen@gmail.com>
2023-05-03 20:52:31 +0300

    build

--- src/api/api.ts
+++ src/api/api.ts
@@ -2,16 +2,17 @@ import axios from 'axios';
 import { UserType } from '../redux/features/authSlice';
 const instance = axios.create({
   baseURL: 'https://a17339-cb01.f.d-f.pw',
-  headers: {
-    Authorization: `token ${localStorage.getItem('token')}`,
-  },
 });
 export const api = {
   getCars() {
     return instance.get(`/cars/`);
   },
   checkLogin() {
-    return instance.get(`/auth/me/`);
+    return instance.get(`/auth/me/`, {
+      headers: {
+        Authorization: `token ${localStorage.getItem('token')}`,
+      },
+    });
   },
   login(userData: { username: string; password: string }) {
     return instance.post(`/auth/login/`, userData);
